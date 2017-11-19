bda4db774e3da9e3770030a9f33739f552ac1fa0
Mikael Utterback <mikaelutterback@outlook.com>
2017-11-19 17:11:04 +0100

    S2 Finalized

--- resources/function/func-login.php
+++ resources/function/func-login.php
@@ -1,37 +1,37 @@
 <?php
-    session_start();
-    $_SESSION['usernameError'] = $_SESSION['passwordError'] = $_SESSION['otherError'] = "";
+session_start();
+$_SESSION['usernameError'] = $_SESSION['passwordError'] = $_SESSION['otherError'] = "";
 
-    if (isset($_POST['submitLogin'])) {
-        include 'func-dhb-connect.php';
-        $userName = mysqli_real_escape_string($conn, $_POST['userName']);
-        $password = mysqli_real_escape_string($conn, $_POST['userPassword']);
+if (isset($_POST['submitLogin'])) {
+    include 'func-dhb-connect.php';
+    $userName = mysqli_real_escape_string($conn, $_POST['userName']);
+    $password = mysqli_real_escape_string($conn, $_POST['userPassword']);
 
 
-        if (empty($userName) || empty($password)) {
-            if(empty($userName)) { $_SESSION['usernameError'] = "Username is required";}
-            if(empty($password)) { $_SESSION['passwordError'] = "Password is required"; }
+    if (empty($userName) || empty($password)) {
+        if(empty($userName)) { $_SESSION['usernameError'] = "Username is required";}
+        if(empty($password)) { $_SESSION['passwordError'] = "Password is required"; }
 
-            header("Location: ../../login.php?login=emptyfield");
+        header("Location: ../../login.php?login=emptyfield");
+        exit();
+
+    } else {
+        $sqlLine = "SELECT * FROM users WHERE user_name='$userName' AND user_password='$password'";
+        $result = mysqli_query($conn, $sqlLine);
+        $numRows = mysqli_num_rows($result);
+
+        if ($numRows < 1) {
+            $_SESSION['otherError'] = "The username and/or password was incorrect";
+            header("Location: ../../login.php?login=nosuchentry");
             exit();
 
         } else {
-            $sqlLine = "SELECT * FROM users WHERE user_name='$userName' AND user_password='$password'";
-            $result = mysqli_query($conn, $sqlLine);
-            $numRows = mysqli_num_rows($result);
-
-            if ($numRows < 1) {
-                $_SESSION['otherError'] = "The username and/or password was incorrect";
-                header("Location: ../../login.php?login=nosuchentry");
-                exit();
-
-            } else {
-                $_SESSION['userId'] = $row['user_id'];
-                $_SESSION['userName'] = $row['user_name'];
-                $_SESSION['userPassword'] = $row['user_password'];
-                header("Location: ../../index.php?login=success");
-                exit();
-            }
+            $_SESSION['userId'] = $row['user_id'];
+            $_SESSION['userName'] = $row['user_name'];
+            $_SESSION['userPassword'] = $row['user_password'];
+            header("Location: ../../index.php?login=success");
+            exit();
         }
-
     }
+
+}
