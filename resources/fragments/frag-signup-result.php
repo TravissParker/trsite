a65bd8a070f37a790a26441ff6de0b3bbf6dea18
Mikael Utterback <mikaelutterback@outlook.com>
2017-11-19 16:54:26 +0100

    S2 Finalized

--- resources/fragments/frag-signup-result.php
+++ resources/fragments/frag-signup-result.php
@@ -1,7 +1,7 @@
 <?php
 
 if ($_SESSION['signupSuccess'] == true) {
-    echo "Sign up was succesfull!<br />Use your credentials to sign up above.";
+    echo "Sign up was succesfull!<br />Use your credentials to sign in above.";
 } else {
     echo '<form action="../../signup.php">
             <p>Not a member? Click the button below to sign up!</p>
